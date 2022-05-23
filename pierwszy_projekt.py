d6c5f3f2c78717948938972a6c068e563bb7d3d3
mateuszsrebniak <104792164+mateuszsrebniak@users.noreply.github.com>
2022-05-23 21:06:04 +0200

    Płatności i całe zamówienie
    Wprowadzenie płatności i sprawdzania całości zamówienia

--- pierwszy_projekt.py
+++ pierwszy_projekt.py
@@ -1,6 +1,26 @@
+hamburger = ['Hamburger', 15.99]
+cheeseburger = ['Cheeseburger', 17.99]
+bekonburger = ['Bekon Burger', 21.99]
+podwbekburger = ['Podwójny Bekon Burger', 27.99]
+frytkiXXL = ['Frytki XXL', 8.99]
+frytkimale = ['Frytki klasyczne', 4.99]
+krazkiceb = ['Krążki cebulowe 9 szt.', 7.99]
+cola_05 = ['Coca-Cola 0,5', 5.99]
+fanta_05 = ['Fanta', 5.99]
+
+cale_zamowienie = []
+suma_zamowienia = []
+
+
+def dodaj_do_zamowienia(produkt):
+    cale_zamowienie.append(produkt)
+    suma_zamowienia.append(produkt[1])
+    print('Dodano do zamówienia: ', produkt[0])
+
+
 imię = input('Jak masz na imię? ')
 print()
-print('Cześć', imię,'!')
+print('Cześć', imię, '!')
 
 while True:
     print()
@@ -24,6 +44,8 @@ while True:
             print('2. Frytki')
             print('3. Przekąski')
             print('4. Napoje')
+            print('5. Pokaż moje zamówienie')
+            print('6. Do kasy')
             kategoria = int(input('Wybierz kategorię z menu: '))
 
             if kategoria == 0:
@@ -37,11 +59,19 @@ while True:
                     print('2. Cheeseburger')
                     print('3. Bekon Burger')
                     print('4. Podwójny Bekon Burger')
-                    wybrany_produkt1 = int(input('Wybierz produkt: '))
+                    wybrany_produkt = int(input('Wybierz produkt: '))
 
-                    if wybrany_produkt1 == 0:
+                    if wybrany_produkt == 0:
                         break
-        
+                    if wybrany_produkt == 1:
+                        dodaj_do_zamowienia(hamburger)
+                    if wybrany_produkt == 2:
+                        dodaj_do_zamowienia(cheeseburger)
+                    if wybrany_produkt == 3:
+                        dodaj_do_zamowienia(bekonburger)
+                    if wybrany_produkt == 4:
+                        dodaj_do_zamowienia(podwbekburger)
+
             if kategoria == 2:
                 print()
                 print('Frytki')
@@ -49,10 +79,14 @@ while True:
                     print('0. Powrót do wyboru kategorii')
                     print('1. Frytki XXL')
                     print('2. Frytki klasyczne')
-                    wybrany_produkt2 = int(input('Wybierz produkt: '))
+                    wybrany_produkt = int(input('Wybierz produkt: '))
 
-                    if wybrany_produkt2 == 0:
+                    if wybrany_produkt == 0:
                         break
+                    if wybrany_produkt == 1:
+                        dodaj_do_zamowienia(frytkiXXL)
+                    if wybrany_produkt == 2:
+                        dodaj_do_zamowienia(frytkimale)
 
             if kategoria == 3:
                 print()
@@ -60,10 +94,12 @@ while True:
                 while True:
                     print('0. Powrót do wyboru kategorii')
                     print('1. Krążki cebulowe')
-                    wybrany_produkt3 = int(input('Wybierz produkt: '))
+                    wybrany_produkt = int(input('Wybierz produkt: '))
 
-                    if wybrany_produkt3 == 0:
+                    if wybrany_produkt == 0:
                         break
+                    if wybrany_produkt == 1:
+                        dodaj_do_zamowienia(krazkiceb)
 
             if kategoria == 4:
                 print()
@@ -72,11 +108,41 @@ while True:
                     print('0. Powrót do wyboru kategorii')
                     print('1. Coca-Cola 0,5')
                     print('2. Fanta 0,5')
-                    wybrany_produkt4 = int(input('Wybierz produkt: '))
+                    wybrany_produkt = int(input('Wybierz produkt: '))
 
-                    if wybrany_produkt4 == 0:
+                    if wybrany_produkt == 0:
                         break
-            
+                    if wybrany_produkt == 1:
+                        dodaj_do_zamowienia(cola_05)
+                    if wybrany_produkt == 2:
+                        dodaj_do_zamowienia(fanta_05)
+
+            if kategoria == 5:
+                print()
+                print('Twoje zamówienie: ', cale_zamowienie)
+                print('Kwota zamówienia: ', sum(suma_zamowienia), 'zł')
+                while True:
+                    print()
+                    powrot = int(input("Wciśnij '1', by wrócić do menu: "))
+
+                    if powrot == 1:
+                        break
+
+            if kategoria == 6:
+                print()
+                print('Twoje zamówienie: ', cale_zamowienie)
+                print('Kwota zamówienia: ', sum(suma_zamowienia), 'zł')
+                while True:
+                    print()
+                    print('0. Powrót do wyboru kategorii')
+                    print('1. Zapłać')
+                    platnosc = int(input('To już wszystko?: '))
+
+                    if platnosc == 0:
+                        break
+                    if platnosc == 1:
+                        break
+
     if restauracja == 2:
         print('Twoja restauracja to Burger Queen Katowice Galeria')
     if restauracja == 3:
